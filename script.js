5bcdfcbbfe76b08ae679117773d89080775188f8
Eduard Badea <eduardbadea@outlook.com>
2025-02-18 17:21:20 +0200

    Update script.js
    Repaird some identation problems, removed redundant code and created a new vector array to call "verifyWinner" function only once.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const gameArray = [];
 let currentPlayer = 1;
 let winnerFound = 1;
 let isTrue = false;
+const vectorCoordinates = [[0, 1], [1, 0], [1, 1], [1, -1]];
 
 function createRows() {
     for (let i = 0; i < MAX_ROW; ++i) {
@@ -37,20 +38,17 @@ createCol();
 
 function playerTurn(div, row, col) {
     if (currentPlayer === 1 && checkColor(div) && winnerFound !== 0) {
-        changeColourForPlayer1(div, row, col);
-        console.log(checkWin('R'));
+        changeColourForPlayer(div, row, col, currentPlayer);
         if (checkWin('R')) {
-            document.getElementById('player-move').innerHTML = 'Player 1 WON!';
-            winnerFound = 0;
+            showWinnerMessage(currentPlayer);
             createRestartButton();
         } else {
             playerTurnText(currentPlayer);
         }
     } else if (currentPlayer === 2 && winnerFound !== 0) {
-        changeColourForPlayer2(div, row, col);
+        changeColourForPlayer(div, row, col, currentPlayer);
         if (checkWin('G')) {
-            document.getElementById('player-move').innerHTML  = 'Player 2 WON!';
-            winnerFound = 0;
+            showWinnerMessage(currentPlayer);
             createRestartButton();
         } else {
             playerTurnText(currentPlayer);
@@ -62,7 +60,12 @@ function checkColor(div) {
     return window.getComputedStyle(div).backgroundColor === 'rgb(255, 255, 255)';
 }
 
-function playerTurnText(currentPlayer) {
+function playerTurnText(playerTurn) {
+    if (playerTurn === 1) {
+        currentPlayer = 2;
+    } else {
+        currentPlayer = 1;
+    }
     let playerText = document.getElementById('player-move');
     playerText.innerHTML = 'Player move: Player ' + currentPlayer;
 }
@@ -73,10 +76,9 @@ function checkWin(player) {
             if (gameArray[row][col] !== player) {
                 continue;
             }
-            verifyWinner(row, col, 0, 1);
-            verifyWinner(row, col, 1, 0);
-            verifyWinner(row, col, 1, 1);
-            verifyWinner(row, col, 1, -1);
+            for (const [x, y] of vectorCoordinates) {
+                verifyWinner(row, col, x, y)
+            }
             if (isTrue === true) {
                 isTrue = false;
                 return true;
@@ -86,16 +88,14 @@ function checkWin(player) {
     return false;
 }
 
-function changeColourForPlayer2(player, row, col) {
-    player.style.backgroundColor ="rgb(255, 255, 0)";
-    gameArray[row][col] = 'G';
-    currentPlayer = 1;
-}
-
-function changeColourForPlayer1(player, row, col) {
-    player.style.backgroundColor ="rgb(255, 0, 0)";
-    gameArray[row][col] = 'R';
-    currentPlayer = 2;
+function changeColourForPlayer(player, row, col, playerTurn) {
+    if (playerTurn === 2) {
+        player.style.backgroundColor ="rgb(255, 255, 0)";
+        gameArray[row][col] = 'G';
+    } else {
+        player.style.backgroundColor ="rgb(255, 0, 0)";
+        gameArray[row][col] = 'R';
+    }
 }
 
 function createRestartButton() {
@@ -109,12 +109,20 @@ function createRestartButton() {
 }
 
 function verifyWinner(row, col, rowIndex, colIndex) {
-    if (gameArray[row][col] === gameArray[row + (1 * rowIndex)][col + (1 * colIndex)] &&
-        gameArray[row][col] === gameArray[row + (2 * rowIndex)][col + (2 * colIndex)] &&
-        gameArray[row][col] === gameArray[row + (3 * rowIndex)][col + (3 * colIndex)]) {
-            isTrue = true;
-            return true;
-        }
+    if (gameArray[row][col] === 
+        gameArray[row + (1 * rowIndex)][col + (1 * colIndex)] &&
+        gameArray[row][col] === 
+        gameArray[row + (2 * rowIndex)][col + (2 * colIndex)] &&
+        gameArray[row][col] === 
+        gameArray[row + (3 * rowIndex)][col + (3 * colIndex)]) {
+        isTrue = true;
+        return true;
+    }
+}
+
+function showWinnerMessage(playerTurn) {
+    document.getElementById('player-move').innerHTML = 'Player ' + playerTurn + ' WON!';
+    winnerFound = 0;
 }
 
 function startNewGame() {
